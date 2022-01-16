4d4b3f27af5b191cae4abb5ad088d32234e51eb3
mariam <maryamsajjadian2020@gmail.com>
2022-01-16 18:34:50 +0330

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'map', component: MapComponent },
+  { path: 'about',
+  children:[{
+    path: "contact", component: ContactUsComponent
+  }],
+  component: AboutComponent  },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {path: '**', component:NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MapComponent } from './components/map/map.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MapComponent,
+    HeaderComponent,
+    NotFoundPageComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
